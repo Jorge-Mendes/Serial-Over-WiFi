ecfb99f67193d09261e4876d0164ad01b6903f70
Jorge Mendes <jorge_mendes_1@hotmail.com>
2018-10-07 17:57:01 +0100

    Add interface improvements

--- Serial-Over-WiFi.ino
+++ Serial-Over-WiFi.ino
@@ -2,123 +2,154 @@
  * ESP8266 NodeMCU AJAX Demo
  * Updates and Gets data from webpage without page refresh
  * https://circuits4you.com
- */
+*/
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
- 
-#include "index.h" //Our HTML webpage contents with javascripts
- 
-#define LED 2  //On board LED
 
+// Our HTML webpage contents
+#include "index.h"
 
-String inputString = ""; // Para guardar os caracteres que chegam pela porta série.
-char inChar;
+// On board LED
+#define LED 2
 
+// Save the characters that arrive through the serial port
+String lastReceivedString = "";
+String allReceivedString = "";
+char receivedChar;
 
-IPAddress    apIP(42, 42, 42, 43);  // Defining a static IP address: local & gateway
-                                    // Default IP in AP mode is 192.168.4.1
+// Defining a static IP address: local & gateway
+// Default IP in AP mode is 192.168.4.1
+IPAddress apIP(42, 42, 42, 42);  
 
-const char *ssid = "ESPNUNO";
+// SSID and Password for the Access Point
+const char *ssid = "ESPJORGE";
 const char *password = "123456789";
 
-
+// Server on port 80
+ESP8266WebServer server(80); 
  
-ESP8266WebServer server(80); //Server on port 80
- 
-//===============================================================
-// This routine is executed when you open its IP in browser
-//===============================================================
+// ===================================================================
+// This routine is executed when you open the IP of the AP in browser
+// ===================================================================
 void handleRoot() {
- String s = MAIN_page; //Read HTML contents
- server.send(200, "text/html", s); //Send web page
+  // On Windows it works like this:
+  //String s = MAIN_page;               // Read HTML contents
+  //server.send(200, "text/html", s);   // Send web page
+
+  // On Linux works like this:
+  PGM_P s = MAIN_page;                  // Read HTML contents
+  server.send_P(200, "text/html", s);   // Send web page
 }
 
-
-// Esta função vai ser chamada de 2 em 2 segundos porque 
-// no javascript não existe uma acção para chamar a função
-// então ela é chamada com a função setInterval(function()). 
-void handleADC() { 
-
-  while (Serial.available()) {        
-    inChar = (char)Serial.read();
-    inputString += inChar;      
+// ===================================================================
+// This routine is responsible for reading what was received on the
+// serial port and put this in the received messages box of the web
+// page
+// This function will be called every 100 milliseconds because in
+// javascript there is no action to call the function so it is called
+// with the function setInterval(function()) in the index.h
+// ===================================================================
+void handleReceive() {
+  while (Serial.available()) {
+    receivedChar = (char)Serial.read();
+    lastReceivedString += receivedChar;
   }
-
-  // inputString += "<br>"; // Nem pensar
-   
-  server.send(200, "text/plane", inputString); 
-
-  //inputString = ""; // Não é uma boa ideia. Aparece texto mas com o refresh de 2 s o texto tem que ser rápido.
-}
- 
-void handleLED() {
- String ledState = "OFF";
- String t_state = server.arg("LEDstate"); //Refer  xhttp.open("GET", "setLED?LEDstate="+led, true);
- //Serial.println(t_state);
- if(t_state == "1")
- {
-  digitalWrite(LED,LOW); //LED ON
-  ledState = "ON"; //Feedback parameter
- }
- else
- {
-  digitalWrite(LED,HIGH); //LED OFF
-  ledState = "OFF"; //Feedback parameter  
- }
- 
- server.send(200, "text/plane", ledState); //Send web page
+  if (lastReceivedString.length() > 0) {
+    if (allReceivedString.length() == 0) {
+      allReceivedString = allReceivedString + lastReceivedString;
+    }
+    else {
+      allReceivedString = allReceivedString + "\n" + lastReceivedString;
+    }
+    Serial.print("<< ");
+    Serial.println(lastReceivedString);
+  }
+  lastReceivedString = "";
+  server.send(200, "text/plane", allReceivedString); 
 }
 
-
-void handleTexto() {   
-  String texto = server.arg("texto"); //Refer  xhttp.open("GET", "ajax_inputs" + strText, true);
-  Serial.println(texto);
- 
+// ===================================================================
+// This routine is responsible for controlling the ESP8266 LED
+// ===================================================================
+void handleLed() {
+  String ledState = "OFF";
+  String t_state = server.arg("LEDstate");    // Refer: xhttp.open("GET", "setLED?LEDstate="+led, true);
+  if(t_state == "1") {
+    digitalWrite(LED,LOW);                    // Set LED ON
+    ledState = "ON";                          // Feedback parameter
+  }
+  else
+  {
+    digitalWrite(LED,HIGH);                   // Set LED OFF
+    ledState = "OFF";                         // Feedback parameter
+  }
+  server.send(200, "text/plane", ledState);   // Send feedback parameter to the web page
 }
 
-void handleClearTexto() {   
-  String texto = server.arg("texto2"); //Refer  xhttp.open("GET", "ajax_inputs" + strText, true);
-  texto="";
-  inputString = "";
-  server.send(200, "text/plane", texto);
+// ===================================================================
+// This routine is responsible for sending to the serial port what
+// was inserted in the sent messages box of the web page
+// ===================================================================
+void handleSend() {
+  String texto = server.arg("texto");         // Refer: xhttp.open("GET", "ajax_inputs" + strText, true);
+  Serial.print(">> ");
+  Serial.println(texto);                      // This line is responsible for sending the message to the serial port
 }
 
+// ===================================================================
+// This routine is responsible for clearing all the received messages
+// and clear the received messages box of the web page
+// ===================================================================
+void handleClear() {
+  //String texto = server.arg("texto2");        // Refer: xhttp.open("GET", "clear_text" + clrText, true);
+  //texto="";
+  allReceivedString = "";
+  server.send(200, "text/plane", allReceivedString);
+}
 
-//==============================================================
-//                  SETUP
-//==============================================================
+// ===================================================================
+// Setup routine
+// ===================================================================
 void setup(void){
   delay(1000);
-  //Onboard LED port Direction output
-  pinMode(LED,OUTPUT);
-
-  Serial.begin(9600);
-  Serial.println();
-  Serial.println("Configuring access point...");
-
-  //set-up the custom IP address
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));   // subnet FF FF FF 00 
   
+  // Onboard LED port set to OUTPUT
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,HIGH);
+  
+  Serial.begin(9600);
+  
+  Serial.println("\nConfiguring access point...");
+
+  // Set-up the custom IP address
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));   // Subnet FF FF FF 00
   WiFi.softAP(ssid, password);
-  // Só para saber que lhe foi atribuido um IP
+  
+  // IP address assigned to your ESP8266
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
  
-  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
-  server.on("/setLED", handleLED);
-  server.on("/readADC", handleADC);
-  server.on("/ajax_inputs", handleTexto);
-  server.on("/clear_text", handleClearTexto);
- 
-  server.begin();                  //Start server
-  Serial.println("HTTP server started");
+  server.on("/", handleRoot);         // Which routine to handle at root location. This is display page
+  server.on("/setLED", handleLed);
+  server.on("/readADC", handleReceive);
+  server.on("/ajax_inputs", handleSend);
+  server.on("/clear_text", handleClear);
+  
+  // Start server
+  server.begin();
+  Serial.println("HTTP server started!");
 }
-//==============================================================
-//                     LOOP
-//==============================================================
-void loop(void){
-  server.handleClient();          //Handle client requests
+
+// ===================================================================
+// Loop routine
+// ===================================================================
+void loop(void) {
+  // Handle client requests
+  server.handleClient();
 }
+
+// Source: https://circuits4you.com/2016/12/16/esp8266-web-server-html/
--- index.h
+++ index.h
@@ -1,40 +1,324 @@
 const char MAIN_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
+<head>
+
+
+
+<style>
+
+input[type=submit] {
+    background-color: #2196F3;
+    border: none;
+    color: white;
+    padding: 7px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 6px;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {display:none;}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+
+
+/* ------ */
+
+body {
+  background: #FFF;
+}
+
+/* Reset Select */
+select {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -ms-appearance: none;
+  appearance: none;
+  outline: 0;
+  box-shadow: none;
+  border: 0 !important;
+  background: #2196F3;
+  background-image: none;
+  font-size: 16px;
+}
+/* Custom Select */
+.select {
+  position: relative;
+  display: block;
+  <!-- width: 370px; -->
+  width: 100%;
+  height: 50px;
+  line-height: 3;
+  background: #2196F3;
+  overflow: hidden;
+  border-radius: 6px;
+  margin: 0px 2px;
+}
+select {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0 0 0 15px;
+  color: #FFF;
+  cursor: pointer;
+}
+select::-ms-expand {
+  display: none;
+}
+/* Arrow */
+.select::after {
+  content: '\25BC';
+  position: absolute;
+  top: 0;
+  right: 0;border:solid 1px gray
+  bottom: 0;
+  padding: 0 1em;
+  background: #2196F3;
+  pointer-events: none;
+  color: #FFF;
+}
+/* Transition */
+.select:hover::after {
+  color: #FFF;
+}
+.select::after {
+  -webkit-transition: .25s all ease;
+  -o-transition: .25s all ease;
+  transition: .25s all ease;
+}
+
+/* ------ */
+
+
+.control {
+  font-size: 18px;
+  position: relative;
+  display: block;
+  margin-bottom: 15px;
+  padding-left: 30px;
+  padding-top: 3px;
+  cursor: pointer;
+}
+
+.control input {
+  position: absolute;
+  z-index: -1;
+  opacity: 0;
+}
+
+.control__indicator {
+  position: absolute;
+  top: 2px;
+  left: 0;
+  width: 20px;
+  height: 20px;
+  background: #2196F3;
+}
+
+/* Checked state */
+.control input:checked ~ .control__indicator {
+  background: #2196F3;
+}
+
+/* Check mark */
+.control__indicator:after {
+  position: absolute;
+  display: none;
+  content: '';
+}
+
+/* Show check mark */
+.control input:checked ~ .control__indicator:after {
+  display: block;
+}
+
+/* Checkbox tick */
+.control--checkbox .control__indicator:after {
+  top: 4px;
+  left: 8px;
+  width: 3px;
+  height: 8px;
+  transform: rotate(45deg);
+  border: solid #fff;
+  border-width: 0 2px 2px 0;
+}
+
+/* ---- */
+
+textarea {
+  border:solid 2px #CCC;
+  width: 98%;
+}
+
+
+</style>
+  <meta name="viewport" content="width=device-width">   <!-- https://stackoverflow.com/a/14581134 -->
+  <div id="serialoverwifi">
+    <h1>Serial over WiFi</h1>
+  </div>
+</head>
+
+
+
 <body>
+  <!--
+    <div>
+      Use this buttons to test WiFi connection with AP<br>
+      <button type="button" onclick="sendData(1)">LED ON</button>
+      <button type="button" onclick="sendData(0)">LED OFF</button><br><br>
+    </div>
+    <div>
+      Verify if NodeMCU LED is ON or OFF<BR>
+      LED State is : <span id="LEDState">NA</span><br><br>
+    </div>
+  -->
+  
+  <div>
+    <h3>Test WiFi connection with NodeMCU using the onboard LED:</h3>
+    LED State is: <span id="LEDState">OFF</span><br>
+    <label class="switch">
+      <input type="checkbox" id="myCheck" onclick="sendData()">   <!--checked-->
+      <span class="slider round"></span>
+    </label><br><br>
+  </div>
 
-<div id="serialoverwifi">
-<h1>Serial over WiFi</h1>
-  Use this buttons to test wifi connection to AP<BR>
-	<button type="button" onclick="sendData(1)">LED ON</button>
-	<button type="button" onclick="sendData(0)">LED OFF</button><BR><BR>
-</div>
+  <div>
+    <h3>Predefined commands to send:</h3>
+    <form class="select" id="commands_form" name="frmCommands">
+      <select id="commands">
+        <option value="comando_def" disabled="disabled" selected="selected">Select one command</option>
+        <option value="comando_1">Comando 1 dasdsadas das d asd as das d as</option>
+        <option value="comando_2">Comando 2</option>
+        <option value="comando_3">Comando 3</option>
+        <option value="comando_4">Comando 4</option>
+        <option value="comando_5">Comando 5</option>
+      </select>
+    </form>
+    <input type="submit" value="Send" onclick="SendCommand()"/><br><br>
+  </div>
 
-<div>
-  Verify if NodeMCU LED is ON or OFF<BR>
-  LED State is : <span id="LEDState">NA</span><BR><BR>
-</div>
+  <div>
+    <h3>Text or command to send:</h3>
+	  <form id="txt_form" name="frmText">
+      <textarea name="form_text" rows="1"></textarea><br>    <!-- cols="45" -->
+    </form>
+    <input type="submit" value="Send" onclick="SendText()"/><br><br>
+  </div>
 
-
-<div>
-  <h3>Text or commands to send</h3>
-	<form id="txt_form" name="frmText">
-     <textarea name="form_text" rows="10" cols="40"></textarea><BR>
-  </form>
-  <input type="submit" value="Send" onclick="SendText()" /><BR><BR>
-</div>
-
-
-<div>
-	<h3>Received text or commands</h3> 
-	<form id="ADCValue" name="frmText2">
-        <textarea readonly="true" name="form_text2" rows="10" cols="40" style="overflow:auto">0</textarea><BR>
-  </form>	
-  <input type="submit" value="Clear screen" onclick="ClearText()" /><BR>    
-</div>
+  <div>
+	  <h3>Received text or commands:</h3> 
+	  <form id="ADCValue" name="frmText2">
+      <textarea name="form_text2" rows="10" readonly="true" style="overflow:auto;">Starting...</textarea><br>    <!-- cols="45" -->
+    </form>
+    <div style="width: 100%; overflow: hidden;">
+      <div style="width: 100px; float: left;">
+        <input type="submit" value="Clear screen" onclick="ClearText()"/>
+      </div>
+      <div style="margin-left: 170px; margin-top: 10px;">
+        <label class="control control--checkbox">Autoscroll
+          <input id="autoscroll" type="checkbox" checked="checked"/>
+          <div class="control__indicator"></div>
+        </label>
+      </div>
+    </div>
+    <br>
+  </div>
 
 <script>
-function sendData(led) {
+
+
+
+function sendData() {
+  led = 0
+  led_state = ""
+  var xhttp = new XMLHttpRequest();
+
+
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    led = 1
+    led_state = "ON"
+  } else {
+    led = 0
+    led_state = "OFF"
+  }
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("LEDState").innerHTML =
+      led_state;
+    }
+  };
+
+  
+  xhttp.open("GET", "setLED?LEDstate="+led, true);
+  xhttp.send();
+}
+
+
+
+
+function sendDatax(led) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -46,10 +330,12 @@ function sendData(led) {
   xhttp.send();
 }
 
+
+
 setInterval(function() {
   // Call a function repetatively with 2 Second interval
   getData();
-}, 2000); //2000mSeconds update rate
+}, 100); //2000mSeconds update rate
 
 function getData() {
   var xhttp = new XMLHttpRequest();
@@ -59,6 +345,13 @@ function getData() {
       this.responseText;
     }
   };
+
+  if(document.getElementById("autoscroll").checked == true) {
+    if(document.getElementById("ADCValue").form_text2.selectionStart == document.getElementById("ADCValue").form_text2.selectionEnd) {
+      document.getElementById("ADCValue").form_text2.scrollTop = document.getElementById("ADCValue").form_text2.scrollHeight;
+    }
+  }
+  
   xhttp.open("GET", "readADC", true);
   xhttp.send();
 }
@@ -73,6 +366,16 @@ function SendText()
 	
 	xhttp.open("GET", "ajax_inputs" + strText, true);
 	xhttp.send(null);
+}
+
+
+function SendCommand()
+{
+  var xhttp = new XMLHttpRequest();
+  var e = document.getElementById("commands")
+  strText = "?texto=" + e.options[e.selectedIndex].text;;
+  xhttp.open("GET", "ajax_inputs" + strText, true);
+  xhttp.send(null);
 }
 
 
